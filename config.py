376e4c54963add88330e7b6773b3b48ecd9b1bc2
zokirbek85 <82650730+zokirbek85@users.noreply.github.com>
2025-07-12 02:34:22 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,8 @@
-import os
+# config.py
+BOT_TOKEN = '7953315666:AAHkoR0cCSl7jPDvPpFV8dEJJHUj4AOg7sI'
 
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+MANAGERS_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1EbTNuEAtldZ5KIoya9m05ryNREfpgaXolDf_TNH1tPs'
+PRODUCTS_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1yIawwoRoJrsA-5WwCizGRPm05hh9WWdW_2xyYinZGZI'
+ORDERS_SHEET_URL = 'https://docs.google.com/spreadsheets/d/1I2mgQHFqdFcRnVt0D4WczGKOztn-7XPauB7NR33fX0I'
 
-# Google Sheets credentials.json ni to'g'ridan-to'g'ri string ko'rinishida env dan o'qib olish
-import json
-import tempfile
-
-credentials_str = os.getenv("SHEET_CREDENTIALS_JSON")
-with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as temp_file:
-    temp_file.write(credentials_str.encode())
-    GOOGLE_CREDENTIALS_PATH = temp_file.name
+GOOGLE_CREDENTIALS_FILE = 'google_credentials.json'
